c4f5ed5246e36c1403bd7f75d5b0f64336874a96
Bat-Ochir <amgalanenerel@gmail.com>
2020-10-29 00:16:04 +0800

    Importing modules

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 google_rating = pd.read_csv("googleplaystore.csv")
 google_rating_top = google_rating.head()
@@ -10,7 +12,7 @@ print(google_rating_top)
 X = google_rating.drop(columns=['Rating'])
 y = google_rating['Rating']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 1)
 
 dtmodel = DecisionTreeClassifier()
 dtmodel.fit(X_train, y_train)
