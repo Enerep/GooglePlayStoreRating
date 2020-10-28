9890a2a84205603e3f214e3c5f2ac41d24e2c25d
Bat-Ochir <amgalanenerel@gmail.com>
2020-10-28 23:52:46 +0800

    Fixing the dtmodel

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 dtmodel = DecisionTreeClassifier()
 dtmodel.fit(X_train, y_train)
 
-predictions = model.predict(X_test)
+predictions = dtmodel.predict(X_test)
 
 #Outputting the score
 score = accuracy_score(y_test, predictions)
